2948f481cf03eb028d71cb4ba48b83a64d8d65d0
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2025-01-09 01:27:30 +0500

    MoveApp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Artical from './Artical/Artical';
+import Header_Add from './header';
 //import img from './public';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello ReactJS </h1>
-        <p>
-          Welcome to <code>src/App.js</code> and id Home.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/okovtun/PD_321_WEB/tree/master/React"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Terminator
-        </a>
-        <p>
-
-        </p>
-      </header>
+      <Header_Add></Header_Add>
       <Artical> 
       </Artical>
     </div>
