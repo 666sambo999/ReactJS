c4c2140b728fc56900650b9c2bc0b08f6dad0d1f
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2025-01-09 00:51:57 +0500

    Init

---
+++ src/Artical/Artical.js
@@ -0,0 +1,14 @@
+function Artical()
+{
+    return <article>
+    <p>
+      Не удаётся установить соединение с сайтом.
+      Pageclip is a simple way to save information from your website via forms or front-end JavaScript. 
+      That is, you can save data from your website without a server—Pageclip is your server.
+    </p>
+    <p>
+      lorem*5 = tab(будет много текста:)
+    </p>
+  </article>
+}
+export default Artical;
